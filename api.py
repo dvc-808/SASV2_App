f91e3cccaaf4ae76b8f43f2dd7072187cdaab907
dvc <dvcchamhoc@gmail.com>
2025-03-26 13:13:55 +0700

    first commit

---
+++ api.py
@@ -0,0 +1,22 @@
+from typing import Union
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.post("/register")
+async def register_spk():
+    return {"Hello": "World"}
+
+@app.post("/verify")
+async def verify():
+    return {":"}
+
+
+@app.post("/spoof-detect")
+async def spoofDetect():
+    return {":"}
+
+@app.post("/uploadAudio")
+async def register_spk():
+    return {"Hello": "World"}
---
+++ infer.py
@@ -0,0 +1,6 @@
+from DatasetLoader import test_dataset_loader
+from SASVNet import SASVNet 
+
+class Inference():
+    def __init__(self):
+        
