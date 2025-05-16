14f3476ce2ca0e7ec65d73b97f6ad6dd17c66acc
dvc <dvcchamhoc@gmail.com>
2025-05-16 09:42:49 +0700

    sua ten save file

--- trainSASVNet.py
+++ trainSASVNet.py
@@ -55,6 +55,7 @@ parser.add_argument('--num_class',      type=int,   default=41,     help='Number
 ## Load and save
 parser.add_argument('--initial_model',  type=str,   default="",     help='Initial model weights')
 parser.add_argument('--result_save_path',      type=str,   default="./exp",     help='Path for model and logs')
+parser.add_argument('--result_file_name',      type=str,   default="",     help='Path for model and logs')
 
 ## Training and test data
 parser.add_argument('--train_list',     type=str,   default="",     help='Train list')
@@ -95,7 +96,7 @@ def main_worker(args):
     it = 1
     SASV_EERs, SV_EERs, SPF_EERs = [], [], []
     ## Write args to scorefile
-    scorefile   = open(args.result_save_path+"/scores.txt", "a+")
+    scorefile   = open(args.result_save_path+"/"+args.result_file_name+"/scores.txt", "a+")
     ## Print params
     pytorch_total_params = sum(p.numel() for p in s.module.__S__.parameters())
     print('Total parameters: {:.2f}M'.format(float(pytorch_total_params)/1024/1024))
